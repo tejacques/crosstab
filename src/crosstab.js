da6cf77905fefe36e105bdec7c77cf2ddee98dc4
Tom Jacques <tejacques@gmail.com>
2014-08-29 17:22:24 -0400

    Detect frozen tab environments and drop support for crosstab when detected. Closes #1

--- src/crosstab.js
+++ src/crosstab.js
@@ -4,7 +4,11 @@ var crosstab = (function () {
     // See: http://detectmobilebrowsers.com/about
     var useragent = navigator.userAgent || navigator.vendor || window.opera;
     window.isMobile = (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(useragent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(useragent.substr(0, 4)));
+
     var localStorage = window.localStorage;
+
+    // Other reasons
+    var frozenTabEnvironment = false;
 
     function notSupported() {
         var errorMsg = 'crosstab not supported';
@@ -17,6 +21,9 @@ var crosstab = (function () {
         }
         if (window.isMobile) {
             reasons.push('mobile browser');
+        }
+        if (frozenTabEnvironment) {
+            reasons.push('frozen tab environment detected');
         }
 
         if(reasons.length > 0) {
@@ -31,7 +38,9 @@ var crosstab = (function () {
         keys: {
             MESSAGE_KEY: 'crosstab.MESSAGE_KEY',
             TABS_KEY: 'crosstab.TABS_KEY',
-            MASTER_TAB: 'MASTER_TAB'
+            MASTER_TAB: 'MASTER_TAB',
+            SUPPORTED_KEY: 'crosstab.SUPPORTED',
+            FROZEN_TAB_ENVIRONMENT: 'crosstab.FROZEN_TAB_ENVIRONMENT'
         }
     };
 
@@ -209,7 +218,8 @@ var crosstab = (function () {
     function setLocalStorageItem(key, data) {
         var storageItem = {
             id: crosstab.id,
-            data: data
+            data: data,
+            timestamp: util.now()
         };
 
         localStorage.setItem(key, JSON.stringify(storageItem));
@@ -362,18 +372,62 @@ var crosstab = (function () {
     }
 
     // ---- Return ----
-    var crosstab = {
-        id: util.generateId(),
-        supported: !!localStorage && window.addEventListener && !window.isMobile,
-        util: util,
-        broadcast: broadcast,
-        broadcastMaster: broadcastMaster
+    var setupComplete = false;
+    util.events.once('setupComplete', function () {
+        setupComplete = true;
+    });
+
+    var crosstab = function (fn) {
+        if (setupComplete) {
+            fn();
+        } else {
+            util.events.once('setupComplete', fn);
+        }
     };
 
+    crosstab.id = util.generateId();
+    crosstab.supported = !!localStorage && window.addEventListener && !window.isMobile;
+    crosstab.util = util;
+    crosstab.broadcast = broadcast;
+    crosstab.broadcastMaster = broadcastMaster;
+
+    // --- Crosstab supported ---
+    // Check to see if the global supported key has been set.
+    if (!setupComplete) {
+        var supportedRaw = getLocalStorageRaw(util.keys.SUPPORTED_KEY);
+        var supported = supportedRaw.data;
+        if (supported === false || supported === true) {
+            // As long as it is explicitely set, use the value
+            crosstab.supported = supported;
+            util.events.emit('setupComplete');
+        }
+    }
+
+    // Check to see if the global frozen tab environment key has been set.
+    if (!setupComplete && crosstab.supported) {
+        var frozenTabsRaw = getLocalStorageRaw(util.keys.FROZEN_TAB_ENVIRONMENT);
+        var frozenTabs = frozenTabsRaw.data;
+        if (frozenTabs === true) {
+            frozenTabEnvironmentDetected();
+            util.events.emit('setupComplete');
+        }
+    }
+
+    function frozenTabEnvironmentDetected() {
+        crosstab.supported = false;
+        frozenTabEnvironment = true;
+        setLocalStorageItem(util.keys.FROZEN_TAB_ENVIRONMENT, true);
+        setLocalStorageItem(util.keys.SUPPORTED_KEY, false);
+        crosstab.broadcast = notSupported;
+    }
+
     // --- Tab Setup ---
-    // 5 second timeout
+    // 3 second keepalive
     var TAB_KEEPALIVE = 3 * 1000;
+    // 5 second timeout
     var TAB_TIMEOUT = 5 * 1000;
+    // 100 ms ping timeout
+    var PING_TIMEOUT = 100;
 
     function getStoredTabs() {
         var storedTabs = getLocalStorageItem(util.keys.TABS_KEY);
@@ -409,6 +463,59 @@ var crosstab = (function () {
         util.forEach(deadTabs, function (tab) {
             broadcast(util.eventTypes.tabClosed, tab.id);
         });
+
+        // check to see if setup is complete
+        if (!setupComplete) {
+            var masterTab = crosstab.util.tabs[crosstab.util.keys.MASTER_TAB];
+            // ping master
+            if (masterTab && masterTab.id !== myTab.id) {
+                var timeout;
+                var start;
+
+                crosstab.util.events.once('PONG', function () {
+                    if (!setupComplete) {
+                        clearTimeout(timeout);
+                        // set supported to true / frozen to false
+                        setLocalStorageItem(
+                            util.keys.SUPPORTED_KEY,
+                            true);
+                        setLocalStorageItem(
+                            util.keys.FROZEN_TAB_ENVIRONMENT,
+                            false);
+                        util.events.emit('setupComplete');
+                    }
+                });
+
+                start = util.now();
+
+                // There is a nested timeout here. We'll give it 100ms
+                // timeout, with iters "yields" to the event loop. So at least
+                // iters number of blocks of javascript will be able to run
+                // covering at least 100ms
+                var recursiveTimeout = function (iters) {
+                    var diff = util.now() - start;
+
+                    if (setupComplete) {
+                        // do nothing
+                    } else if (iters <= 0 && diff > PING_TIMEOUT) {
+                        frozenTabEnvironmentDetected();
+                        util.events.emit('setupComplete');
+                    } else {
+                        timeout = setTimeout(function () {
+                            recursiveTimeout(iters - 1);
+                        }, 5);
+                    }
+                };
+
+                var iterations = 5;
+                timeout = setTimeout(function () {
+                    recursiveTimeout(5);
+                }, PING_TIMEOUT - 5 * iterations);
+                crosstab.broadcastMaster('PING');
+            } else if (masterTab && masterTab.id === myTab.id) {
+                util.events.emit('setupComplete');
+            }
+        }
     }
 
     function keepaliveLoop() {
@@ -425,6 +532,17 @@ var crosstab = (function () {
         // ---- Setup Storage Listener
         window.addEventListener('storage', onStorageEvent, false);
         window.addEventListener('beforeunload', beforeUnload, false);
+
+        util.events.on('PING', function (message) {
+            // only handle direct messages
+            if (!message.destination || message.destination !== crosstab.id) {
+                return;
+            }
+
+            if (util.now() - message.timestamp < 100) {
+                crosstab.broadcast('PONG', null, message.origin);
+            }
+        });
 
         keepaliveLoop();
     }
