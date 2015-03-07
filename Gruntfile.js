67a497303619b54c754d5c08969040aae3acde89
Tom Jacques <tejacques@gmail.com>
2015-03-06 21:38:01 -0500

    Add tests using mocha-phantomjs. Closes #8

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,10 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
-            files: ['Gruntfile.js', 'src/*.js']
+            files: ['Gruntfile.js', 'src/*.js', 'test/**.js'],
+            options: {
+                laxbreak: true
+            }
         },
         watch: {
             scripts: {
@@ -18,11 +21,22 @@ module.exports = function (grunt) {
                     base: '.'
                 }
             }
+        },
+        mocha_phantomjs: {
+            test: {
+                options: {
+                    urls: [
+                        'http://localhost:9000/test/mocha_test.html'
+                    ]
+                }
+            }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-mocha-phantomjs');
 
     grunt.registerTask('default', ['connect', 'watch']);
+    grunt.registerTask('test', ['connect', 'mocha_phantomjs']);
 };
--- test/test-crosstab.js
+++ test/test-crosstab.js
@@ -1,3 +1,73 @@
-describe('crosstab', function() {
-    it('should receive broadcast', function() {});
+describe('crosstab', function () {
+    var runInIframe = function (iframe, fn) {
+        /*jshint evil:true*/
+        var args = [].slice.call(arguments, 2);
+        return iframe.contentWindow.window.eval("("
+            + fn
+            + ").apply(window, " + JSON.stringify(args) + ");");
+    };
+
+    it ('should be a function', function () {
+        expect(window.crosstab).to.be.a('function');
+    });
+    it('should setup without issue', function (done) {
+        crosstab(function() {
+            expect(crosstab.supported).to.be(true);
+            done();
+        });
+    });
+    it('should receive broadcast when sent', function () {
+        var msg = "TestMessage";
+        var received;
+        crosstab.once('test', function(message) {
+            received = message.data;
+        });
+        crosstab.broadcast('test', msg);
+        expect(received).to.be(msg);
+    });
+    it('should only trigger callback once for crosstab.once', function () {
+        var msg = "TestOnce";
+        var received;
+        crosstab.once('testOnce', function (message) {
+            received = message.data;
+        });
+        crosstab.broadcast('testOnce', msg);
+        expect(received).to.be(msg);
+        received = undefined;
+        crosstab.broadcast('testOnce', msg);
+        expect(received).to.be(undefined);
+    });
+    it('should receive broadcasts from other tabs', function (done) {
+        var msg = "OtherTabTestMessage";
+        var received;
+        crosstab.once('otherTabTest', function (message) {
+            received = message.data;
+            expect(received).to.be(msg);
+            done();
+        });
+
+        // create iframe
+        var iframe = document.createElement('iframe');
+        iframe.style.display = 'none';
+        iframe.id = 'iframe';
+        iframe.src = "/test/iframe.html";
+
+
+        
+        iframe.addEventListener('load', function () {
+            // add crosstab script
+            runInIframe(iframe, function (msg) {
+                var crosstabScript = document.createElement('script');
+                crosstabScript.type = 'text/javascript';
+                crosstabScript.src = '/src/crosstab.js';
+                crosstabScript.onload = function () {
+                    crosstab(function () {
+                        crosstab.broadcast('otherTabTest', msg);
+                    });
+                };
+                document.head.appendChild(crosstabScript);
+            }, msg);
+        });
+        document.body.appendChild(iframe);
+    });
 });
