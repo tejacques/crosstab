bca74387ad73f1499d77a56f438bf71145fe3e51
Tom Jacques <tejacques@gmail.com>
2015-03-09 23:52:01 -0400

    Saucelabs testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,20 @@
 module.exports = function (grunt) {
+    var browsers = [{
+        browserName: "firefox",
+        version: "19",
+        platform: "XP"
+    }, {
+        browserName: "googlechrome",
+        platform: "XP"
+    }, {
+        browserName: "googlechrome",
+        platform: "linux"
+    }, {
+        browserName: "internet explorer",
+        platform: "WIN8",
+        version: "10"
+    }];
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
@@ -30,13 +46,28 @@ module.exports = function (grunt) {
                     ]
                 }
             }
-        }
+        },
+        'saucelabs-mocha': {
+            all: {
+                options: {
+                    urls: ["http://localhost:9000/test/mocha_test.html"],
+                    tunnelTimeout: 5,
+                    build: process.env.TRAVIS_JOB_ID,
+                    concurrency: 3,
+                    browsers: browsers,
+                    testname: "mocha tests",
+                    tags: ["master"]
+                }
+            }
+        },
     });
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-mocha-phantomjs');
+    grunt.loadNpmTasks('grunt-saucelabs');
 
     grunt.registerTask('default', ['connect', 'watch']);
     grunt.registerTask('test', ['connect', 'mocha_phantomjs']);
+    grunt.registerTask('sauce', ['connect', 'saucelabs-mocha']);
 };
