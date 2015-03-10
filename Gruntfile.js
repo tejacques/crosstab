b862dcdaec42b06dd84f49f4c0b1b8ae481e3b75
Tom Jacques <tejacques@gmail.com>
2015-03-10 02:17:38 -0400

    Update browsers in sauce tests and update mocha test runner page to work with sauce labs.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,40 @@
 module.exports = function (grunt) {
-    var browsers = [{
-        browserName: "firefox",
-        version: "19",
-        platform: "XP"
-    }, {
-        browserName: "googlechrome",
-        platform: "XP"
-    }, {
-        browserName: "googlechrome",
-        platform: "linux"
-    }, {
-        browserName: "internet explorer",
-        platform: "WIN8",
-        version: "10"
-    }];
+    var browsers = {
+        quick: [
+           ['Windows 7', 'chrome', '40'],
+           ['Windows 7', 'firefox', '35'],
+           ['Windows 7', 'internet explorer', '11'],
+           ['OSX 10.10', 'safari', '8']
+        ],
+        ci: [
+           // Chrome
+           ['Linux', 'chrome', '40'],
+           ['OSX 10.10', 'chrome', '40'],
+           ['Windows XP', 'chrome', '40'],
+           ['Windows 7', 'chrome', '40'],
+           ['Windows 8', 'chrome', '40'],
+           ['Windows 8.1', 'chrome', '40'],
+           // Firefox
+           ['Linux', 'firefox', '35'],
+           ['OSX 10.10', 'firefox', '35'],
+           ['Windows XP', 'firefox', '35'],
+           ['Windows 7', 'firefox', '35'],
+           ['Windows 8', 'firefox', '35'],
+           ['Windows 8.1', 'firefox', '35'],
+           // Internet Explorer
+           // ['Windows XP', 'internet explorer', '8'], // Off for now
+           // ['Windows 7', 'internet explorer', '8'], // Off for now
+           ['Windows 7', 'internet explorer', '9'],
+           ['Windows 7', 'internet explorer', '10'],
+           ['Windows 7', 'internet explorer', '11'],
+           ['Windows 8.1', 'internet explorer', '11'],
+           // Safari
+           ['OSX 10.6', 'safari', '5.1'],
+           ['OSX 10.8', 'safari', '6'],
+           ['OSX 10.9', 'safari', '7'],
+           ['OSX 10.10', 'safari', '8']
+        ]
+    };
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -48,18 +69,29 @@ module.exports = function (grunt) {
             }
         },
         'saucelabs-mocha': {
-            all: {
+            ci: {
                 options: {
                     urls: ["http://localhost:9000/test/mocha_test.html"],
                     tunnelTimeout: 5,
                     build: process.env.TRAVIS_JOB_ID,
                     concurrency: 3,
-                    browsers: browsers,
+                    browsers: browsers.ci,
+                    testname: "ci mocha tests",
+                    tags: ["master"]
+                }
+            },
+            quick: {
+                options: {
+                    urls: ["http://localhost:9000/test/mocha_test.html"],
+                    tunnelTimeout: 5,
+                    build: process.env.TRAVIS_JOB_ID,
+                    concurrency: 3,
+                    browsers: browsers.quick,
                     testname: "mocha tests",
                     tags: ["master"]
                 }
             }
-        },
+        }
     });
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -69,5 +101,6 @@ module.exports = function (grunt) {
 
     grunt.registerTask('default', ['connect', 'watch']);
     grunt.registerTask('test', ['connect', 'mocha_phantomjs']);
-    grunt.registerTask('sauce', ['connect', 'saucelabs-mocha']);
+    grunt.registerTask('test:ci', ['connect', 'mocha_phantomjs', 'saucelabs-mocha:ci']);
+    grunt.registerTask('sauce', ['connect', 'saucelabs-mocha:quick']);
 };
